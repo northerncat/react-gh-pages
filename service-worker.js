3b8a55b56e751ee60f78e0a5316d0bcd7ca0dc03
Brad Huang <brad820309@gmail.com>
2018-11-08 10:45:50 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.6afb47dad6f6ec0c09081a297bcd9025.js"
+  "/react-gh-pages/precache-manifest.87e65fd2980a179d7d1118820546871b.js"
 );
 
 workbox.clientsClaim();
